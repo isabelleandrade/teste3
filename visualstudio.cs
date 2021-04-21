fe480b8fb04ea4d9e61049655aab4ca2dd5f23c5
isabelle.andrade <isabelle.andrade@ecolab.com>
2021-04-21 11:16:29 -0300

    adição do arquivo soma

---
+++ visualstudio.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            int Num1 = 3;
+            int Num2 = 4;
+            Console.WriteLine(Num1+ Num2);
+        }
+    }
+
+}
